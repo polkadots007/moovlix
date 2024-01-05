d15dc9e945106ae59ac35cdcd44f7159bd33b626
1950019 <priyansiparida@gmail.com>
2024-01-05 16:26:42 +0530

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // For all styled components:
+    // create classnames from fileName and displayName in development
+    react({
+      babel: {
+        presets: ["@babel/preset-typescript"],
+        plugins: [
+          "@babel/plugin-transform-typescript",
+          [
+            "babel-plugin-styled-components",
+            {
+              ssr: false,
+              pure: true,
+              displayName: true,
+              fileName: false,
+            },
+          ],
+        ],
+      },
+    }),
+  ],
+});
